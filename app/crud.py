47c5ef92e31cb0536cd57f32307f0b8498319c2a
Sergiorg03 <sergiorodriguezprofesional@gmail.com>
2026-02-25 20:49:19 +0100

    FEAT: Creados tests para cuando no hay datos

--- app/crud.py
+++ app/crud.py
@@ -18,13 +18,23 @@ def get_zones(db: Session):
     """
         Función que obtiene todas las zonas existentes en la base de datos.
     """
-    return db.query(models.Zone).all()
+    zonas = db.query(models.Zone).all()
+
+    if not zonas:
+        raise HTTPException(status_code=404, detail="No hay zonas")
+    
+    return zonas
 
 def get_zones_id(db: Session, id: int):
     """
         Función que obtiene la zona con el id indicado.
     """
-    return db.query(models.Zone).filter(models.Zone.id == id).first()
+    zona = db.query(models.Zone).filter(models.Zone.id == id).first()
+
+    if not zona:
+        raise HTTPException(status_code=404, detail="Zona no encontrada")
+    
+    return zona
 
 '''
     Scooters
@@ -33,10 +43,20 @@ def get_scooters(db: Session):
     '''
         Función que obtiene todos los patinetes existentes en la base de datos.
     '''
-    return db.query(models.Scooter).all()
+    patinetes = db.query(models.Scooter).all()
+
+    if not patinetes:
+        raise HTTPException(status_code=404, detail="No hay patinetes")
+    
+    return patinetes
 
 def get_scooters_id(db: Session, id: int):
     '''
         Función que obtiene el patinete con el id indicado.
     '''
-    return db.query(models.Scooter).filter(models.Scooter.id == id).first()
+    patinete = db.query(models.Scooter).filter(models.Scooter.id == id).first()
+
+    if not patinete:
+        raise HTTPException(status_code=404, detail="Patinete no encontrado")
+    
+    return patinete
--- tests/test_main.py
+++ tests/test_main.py
@@ -5,12 +5,50 @@ def test_root(client: TestClient):
     assert response.status_code == 200
     assert response.json() == {"msg": "API funcionando"}
 
+'''
+    TEST ZONAS
+'''
+
 def test_get_zones(client: TestClient):
     response = client.get("/zones/")
     assert response.status_code == 200
     assert isinstance(response.json(), list)
 
-def test_get_scooters(client: TestClient):
+def test_get_zonas_incorr(client: TestClient):
+    response = client.get("/zones/")
+    assert response.status_code == 404
+    assert response.json() == {"detail": "No hay zonas"}
+
+def test_zonas_id_corr(client: TestClient):
+    response = client.get("/zones/1")
+    assert response.status_code == 200
+    assert isinstance(response.json(), dict)
+
+def test_zonas_id_incorr(client: TestClient):
+    response = client.get("/zones/100")
+    assert response.status_code == 404
+    assert response.json() == {"detail": "Zona no encontrada"}
+
+'''
+    TEST SCOOTERS
+'''
+
+def test_get_scooters_corr(client: TestClient):
     response = client.get("/scooters/")
     assert response.status_code == 200
     assert isinstance(response.json(), list)
+
+def test_get_scooters_incorr(client: TestClient):
+    response = client.get("/scooters/")
+    assert response.status_code == 404
+    assert response.json() == {"detail": "No hay patinetes"}
+
+def test_get_scooter_id_corr(client: TestClient):
+    response = client.get("/scooters/1")
+    assert response.status_code == 200
+    assert isinstance(response.json(), dict)
+
+def test_get_scooter_id_incorr(client: TestClient):
+    response = client.get("/scooters/100")
+    assert response.status_code == 404
+    assert response.json() == {"detail": "Patinete no encontrado"}
