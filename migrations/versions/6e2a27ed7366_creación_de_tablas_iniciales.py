24c83f7859c401fe6fe2661e527e538b9862ad8d
Sergio Rodriguez Geniz <sergiorodriguezprofesional@gmail.com>
2026-03-01 16:51:52 +0100

    fix: correccion de las migraciones

--- migrations/versions/6e2a27ed7366_creación_de_tablas_iniciales.py
+++ migrations/versions/6e2a27ed7366_creación_de_tablas_iniciales.py
@@ -17,42 +17,46 @@ down_revision: Union[str, Sequence[str], None] = None
 branch_labels: Union[str, Sequence[str], None] = None
 depends_on: Union[str, Sequence[str], None] = None
 
+def create():
+    if not op.get_context().dialect.has_table(op.get_bind(), 'scooter'):
+        op.create_table(
+            'scooter',
+            sa.Column('id', sa.INTEGER(), autoincrement=True, nullable=False),
+            sa.Column('numero_serie', sa.VARCHAR(), nullable=False),
+            sa.Column('modelo', sa.VARCHAR(), nullable=False),
+            sa.Column('bateria', sa.INTEGER(), nullable=False),
+            sa.Column(
+                'estado',
+                postgresql.ENUM(
+                    'disponible',
+                    'en_uso',
+                    'mantenimiento',
+                    'sin_bateria',
+                    name='scooterstatus'
+                ),
+                nullable=False
+            ),
+            sa.Column('zona_id', sa.INTEGER(), nullable=True),
+            sa.ForeignKeyConstraint(['zona_id'], ['zone.id'], name=op.f('scooter_zona_id_fkey')),
+            sa.PrimaryKeyConstraint('id', name=op.f('scooter_pkey')),
+            sa.UniqueConstraint('numero_serie', name=op.f('scooter_numero_serie_key'))
+        )
+
+    if not op.get_context().dialect.has_table(op.get_bind(), 'zone'):
+        op.create_table(
+            'zone',
+            sa.Column('id', sa.INTEGER(), autoincrement=True, nullable=False),
+            sa.Column('nombre', sa.VARCHAR(), nullable=False),
+            sa.Column('codigo_postal', sa.INTEGER(), nullable=False),
+            sa.Column('limite_velocidad', sa.INTEGER(), nullable=False),
+            sa.PrimaryKeyConstraint('id', name=op.f('zone_pkey'))
+        )
 
 def upgrade() -> None:
     """Upgrade schema."""
-    op.drop_table('scooter')
-    op.drop_table('zone')
+    create()
 
 def downgrade() -> None:
     """Downgrade schema."""
-    op.create_table(
-        'zone',
-        sa.Column('id', sa.INTEGER(), autoincrement=True, nullable=False),
-        sa.Column('nombre', sa.VARCHAR(), nullable=False),
-        sa.Column('codigo_postal', sa.INTEGER(), nullable=False),
-        sa.Column('limite_velocidad', sa.INTEGER(), nullable=False),
-        sa.PrimaryKeyConstraint('id', name=op.f('zone_pkey'))
-    )
-
-    op.create_table(
-        'scooter',
-        sa.Column('id', sa.INTEGER(), autoincrement=True, nullable=False),
-        sa.Column('numero_serie', sa.VARCHAR(), nullable=False),
-        sa.Column('modelo', sa.VARCHAR(), nullable=False),
-        sa.Column('bateria', sa.INTEGER(), nullable=False),
-        sa.Column(
-            'estado',
-            postgresql.ENUM(
-                'disponible',
-                'en_uso',
-                'mantenimiento',
-                'sin_bateria',
-                name='scooterstatus'
-            ),
-            nullable=False
-        ),
-        sa.Column('zona_id', sa.INTEGER(), nullable=True),
-        sa.ForeignKeyConstraint(['zona_id'], ['zone.id'], name=op.f('scooter_zona_id_fkey')),
-        sa.PrimaryKeyConstraint('id', name=op.f('scooter_pkey')),
-        sa.UniqueConstraint('numero_serie', name=op.f('scooter_numero_serie_key'))
-    )
+    op.drop_table('scooter')
+    op.drop_table('zone')
