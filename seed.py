e3bd5aba23c0da895c44c297e0414a8b582683d1
Sergiorg03 <sergiorodriguezprofesional@gmail.com>
2026-03-10 16:28:16 +0100

    Modificados el archivo de insercion de datos de prueba

--- seed.py
+++ seed.py
@@ -1,6 +1,7 @@
 from sqlalchemy.orm import Session
 from app.database import SessionLocal, engine, Base
 from app.models import Zone, Scooter, ScooterStatus
+import traceback
 
 def seed_data():
     # Las tablas se gestionan mediante Alembic
@@ -15,20 +16,20 @@ def seed_data():
         print("Introduciendo datos iniciales...")
 
         # 1. Crear Zonas
-        centro = Zone(nombre="Centro Histórico", codigo_postal=28001, limite_velocidad=20)
-        norte = Zone(nombre="Zona Norte", codigo_postal=28050, limite_velocidad=30)
-        retiro = Zone(nombre="Parque del Retiro", codigo_postal=28009, limite_velocidad=10)
+        centro = Zone(nombre="Centro deresha", codigo_postal=28001, limite_velocidad=20)
+        norte = Zone(nombre="Las 3k", codigo_postal=28050, limite_velocidad=30)
+        retiro = Zone(nombre="Parque de los principes", codigo_postal=28009, limite_velocidad=10)
 
         db.add_all([centro, norte, retiro])
         db.commit() # Commit para obtener los IDs de las zonas
 
         # 2. Crear Patinetes
         patinetes = [
-            Scooter(numero_serie="S001", modelo="Xiaomi M365", bateria=85, estado=ScooterStatus.disponible, zona_id=centro.id),
-            Scooter(numero_serie="S002", modelo="Xiaomi Pro 2", bateria=10, estado=ScooterStatus.sin_bateria, zona_id=centro.id),
-            Scooter(numero_serie="S003", modelo="Ninebot G30", bateria=100, estado=ScooterStatus.en_uso, zona_id=norte.id),
-            Scooter(numero_serie="S004", modelo="Xiaomi M365", bateria=50, estado=ScooterStatus.mantenimiento, zona_id=retiro.id),
-            Scooter(numero_serie="S005", modelo="Ninebot G30", bateria=95, estado=ScooterStatus.disponible, zona_id=retiro.id),
+            Scooter(numero_serie="001", modelo="Chami Pro 2", bateria=85, estado=ScooterStatus.disponible, zona_id=centro.id),
+            Scooter(numero_serie="002", modelo="AMR Patineta", bateria=10, estado=ScooterStatus.sin_bateria, zona_id=centro.id),
+            Scooter(numero_serie="003", modelo="Lacha tarra", bateria=100, estado=ScooterStatus.en_uso, zona_id=norte.id),
+            Scooter(numero_serie="004", modelo="Aquiles Bailo", bateria=50, estado=ScooterStatus.mantenimiento, zona_id=retiro.id),
+            Scooter(numero_serie="005", modelo="Pato lavida", bateria=95, estado=ScooterStatus.disponible, zona_id=retiro.id),
         ]
 
         db.add_all(patinetes)
@@ -36,7 +37,6 @@ def seed_data():
         print("¡Información introducida con éxito!")
 
     except Exception as e:
-        import traceback
         traceback.print_exc()
         db.rollback()
     finally:
